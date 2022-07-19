58b00c8f268a42b1ff2ec8b9581325a1b5b3f294
Денис <den_asy@mail.ru>
2022-07-19 14:07:44 +0300

    миля бонус

---
+++ Java-2.1/src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int ticketprice = 5000;
+        int miles = ticketprice / 20;
+
+        System.out.println("Вы получите бонус " + miles + " миль.");
+
+    }
+}
+
